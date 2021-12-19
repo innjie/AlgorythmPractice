d0738256fc993dd80a71809381dc29fe2f27ed9b
innjie <innjie@naver.com>
2021-12-19 16:36:17 +0900

    [update] baekjoon solve

---
+++ src/com/company/practice/baekjoon/Q1011.java
@@ -0,0 +1,29 @@
+package com.company.practice.baekjoon;
+
+import java.util.Scanner;
+
+public class Q1011 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int times = scan.nextInt();
+
+        for(int i = 0; i < times; i++) {
+            int start = scan.nextInt();
+            int end = scan.nextInt();
+            counting(start, end);
+        }
+    }
+    public static void counting(int start, int end) {
+        int distance = end - start;
+        int count = (int)Math.sqrt(distance);
+
+        if(count == Math.sqrt(distance)) {
+            System.out.println(count * 2 - 1);
+        } else if(distance <= count * count + count) {
+            System.out.println(count * 2);
+        } else {
+            System.out.println(count * 2 + 1);
+        }
+    }
+}
---
+++ src/com/company/practice/baekjoon/Q1978.java
@@ -0,0 +1,26 @@
+package com.company.practice.baekjoon;
+
+import java.util.Scanner;
+
+public class Q1978 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int time = scan.nextInt();
+
+        int count = 0;
+        for(int i = 0; i < time; i++) {
+            count += isPrime(scan.nextInt());
+        }
+        System.out.println(count);
+    }
+    public static int isPrime(int n) {
+        if(n == 1) return 0;
+        for(int i = 2; i < n; i++) {
+            if(n % i == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
