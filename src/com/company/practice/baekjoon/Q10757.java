6378eed27d922456bcc77db5069e24f03ebfdcfd
innjie <innjie@naver.com>
2021-12-19 14:11:06 +0900

    [add] baekjoon practice

---
+++ src/com/company/practice/baekjoon/Q10757.java
@@ -0,0 +1,20 @@
+package com.company.practice.baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ */
+public class Q10757 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        BigInteger A = new BigInteger(scan.next());
+        BigInteger B = new BigInteger(scan.next());
+
+        A = A.add(B);
+
+        System.out.println(A);
+    }
+}
