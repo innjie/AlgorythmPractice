b3e80407e06b47c2cb29d0fd8f8efe4445ff3c34
innjie <innjie@naver.com>
2022-02-25 22:28:32 +0900

    [update] baekjoon solve

---
+++ src/com/company/practice/baekjoon/Q11653.java
@@ -0,0 +1,29 @@
+package com.company.practice.baekjoon;
+
+import java.util.Scanner;
+
+public class Q11653 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int temp = n;
+
+        int i = 2;
+
+        if(n == 1) {
+
+        } else {
+            while(true) {
+                if(i > temp ) break;
+                if(n % i == 0) {
+                    System.out.println(i);
+                    n /= i;
+                } else {
+                    i++;
+                }
+            }
+        }
+
+    }
+}
