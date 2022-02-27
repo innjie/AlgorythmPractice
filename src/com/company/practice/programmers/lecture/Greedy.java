64eff34eff2fbfca2babca5785e4c70750c2216a
innjie <innjie@naver.com>
2022-02-27 23:24:05 +0900

    [update] practice

--- src/com/company/practice/programmers/lecture/Greedy.java
+++ src/com/company/practice/programmers/lecture/Greedy.java
@@ -1,7 +1,51 @@
 package com.company.practice.programmers.lecture;
 
+import java.util.Arrays;
+
 public class Greedy {
     public static void main(String[] args) {
+        int n = 11;
+        int[] stations = {4, 11};
+        int w = 1;
+
+        int maxDistance = w * 2 + 1;
+        int answer = 0;
+
+        int[] isStructed = new int[n + 1];
+
+        for(int i = 0; i < stations.length; i++) {
+            for(int j = stations[i] - w; j < stations[i] + w + 1; j++) {
+                if(j <= n) {
+                    isStructed[j] = 1;
+                }
+
+            }
+        }
+
+        int count = 0;
+        for(int i = 1; i < isStructed.length; i++) {
+            if(isStructed[i] == 0) {
+                count++;
+            } else if(isStructed[i] != 0 && count > 0){
+                answer += count / maxDistance;
+                if(count % maxDistance != 0 )  {
+                    answer++;
+                }
+                count = 0;
+            }
+        }
+
+        if(count > 0) {
+            answer += count / maxDistance;
+            if(count % maxDistance != 0) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+    /*public static void main(String[] args) {
         int n = 20;
         int[] stations = {4, 11};
         int w = 2;
@@ -31,5 +75,5 @@ public class Greedy {
         }
         System.out.println(answer);
 
-    }
+    }*/
 }
