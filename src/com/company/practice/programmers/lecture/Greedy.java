288f1919142f2bff42d41e362b864496e31a430d
innjie <innjie@naver.com>
2022-03-01 23:11:49 +0900

    [update] practice

--- src/com/company/practice/programmers/lecture/Greedy.java
+++ src/com/company/practice/programmers/lecture/Greedy.java
@@ -1,50 +1,50 @@
 package com.company.practice.programmers.lecture;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Greedy {
     public static void main(String[] args) {
-        int n = 11;
-        int[] stations = {4, 11};
-        int w = 1;
+        int n = 13;
+        int[] stations = {2, 3};
+        int w = 5;
 
-        int maxDistance = w * 2 + 1;
         int answer = 0;
 
-        int[] isStructed = new int[n + 1];
 
-        for(int i = 0; i < stations.length; i++) {
-            for(int j = stations[i] - w; j < stations[i] + w + 1; j++) {
-                if(j <= n) {
-                    isStructed[j] = 1;
-                }
+        /*
+            두번째 효율성은 primitive를 object 타입보다 더 빠르게 동작시킬 수 있다.
+            --> station index 사용
+            Queue<Integer> sq = new LinkedList<>();
+            for(int station : stations) {
+                sq.offer(station);
+            }
+         */
 
+
+        int stationIndex = 0;
+        int location = 1;
+        /*시간초과는 루프를 의심하기!!!
+        하지만 이 루프는 중간에 뛰기까지 하기때문에 더 줄이기가 어렵다..*/
+
+        while(location <= n) {
+            if(stationIndex < stations.length && stations[stationIndex] - w <= location) {
+                location = stations[stationIndex] + w + 1;
+                stationIndex++;
+            } else {
+                answer += 1;
+                location += (w * 2 + 1);
             }
         }
-
-        int count = 0;
-        for(int i = 1; i < isStructed.length; i++) {
-            if(isStructed[i] == 0) {
-                count++;
-            } else if(isStructed[i] != 0 && count > 0){
-                answer += count / maxDistance;
-                if(count % maxDistance != 0 )  {
-                    answer++;
-                }
-                count = 0;
-            }
-        }
-
-        if(count > 0) {
-            answer += count / maxDistance;
-            if(count % maxDistance != 0) {
-                answer++;
-            }
-        }
-
         System.out.println(answer);
 
     }
+
+    /**
+     * 새삼 내 처음풀이
+     * 정확도도 떨어지고 많이 느려터졌구나..ㅠㅠ
+     */
     /*public static void main(String[] args) {
         int n = 20;
         int[] stations = {4, 11};
