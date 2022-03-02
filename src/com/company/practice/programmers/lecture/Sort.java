286975dcef5f7e639ee0e8dc53a735f3adadd916
innjie <innjie@naver.com>
2022-03-02 23:06:31 +0900

    [update] practice

--- src/com/company/practice/programmers/lecture/Sort.java
+++ src/com/company/practice/programmers/lecture/Sort.java
@@ -13,12 +13,26 @@ public class Sort {
             numberToString[i] = String.valueOf(n);
             i++;
         }
+        /**
+         * sort는 직접구현보다는 제공하는 라이브러리를 사용하는게 좋음 (OK!)
+         * reverse는 comperator를 사용해야함 -- 변경코드
+         * Arrays.sort(numberToString); -- 기존 코드
+         */
 
-        Arrays.sort(numberToString);
+        Arrays.sort(numberToString, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                //내림차순 정렬
+                return (o1 + o2).compareTo(o2 + o1);
+            }
+        });
 
         String answer = "";
         for(int j = numberToString.length - 1; j >= 0; j--) {
             answer += numberToString[j];
+        }
+        if(answer.charAt(0) == '0') {
+            answer = "0";
         }
         System.out.println(answer);
 
