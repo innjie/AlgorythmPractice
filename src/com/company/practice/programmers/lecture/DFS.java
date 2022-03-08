8cfa9a1aa402e706e381e848f6fe8b1373bf3808
innjie <innjie@naver.com>
2022-03-08 23:31:26 +0900

    [update] practice

--- src/com/company/practice/programmers/lecture/DFS.java
+++ src/com/company/practice/programmers/lecture/DFS.java
@@ -1,32 +1,45 @@
 package com.company.practice.programmers.lecture;
 
+import java.util.Stack;
+
 public class DFS {
-    static int answer = 0;
+    static class P {
+        int open, close;
+        P(int open, int close) {
+            this.open = open;
+            this.close = close;
+        }
+    }
     public static void main(String[] args) {
+        /**
+         * dfs는 stack으로 해결
+         */
+
+        int answer = 0;
         int n = 2;
+        Stack<P> stack = new Stack<>();
+        stack.push(new P(0, 0));
 
-        dfs(n, n);
-        System.out.println(answer);
-    }
-    public static void dfs(int open, int close) {
-        if(open == 0 && close == 0) {
-            answer++;
-            return;
-        }
+        while(!stack.empty()) {
+            P p = stack.pop();
 
-        if(open < close) {
-            if(open != 0) {
-                dfs(open - 1, close);
+            if(p.close > n) {
+                continue;
             }
-            if(close != 0) {
-                dfs(open, close - 1);
+            if(p.open < p.close) {
+                continue;
             }
-        } else { //open >= close
-            dfs(open - 1, close);
+            // 종료조건
+            if(p.open + p.close == n * 2) {
+                answer++;
+                continue;
+            }
 
+            stack.push(new P(p.open + 1, p.close));
+            stack.push(new P(p.open, p.close + 1));
         }
-
     }
+
 
     /**
      * 내 풀이
