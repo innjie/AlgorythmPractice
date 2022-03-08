0e00caa62c76bcb0643d47d1d7af34aa68e142a3
innjie <innjie@naver.com>
2022-03-08 23:26:08 +0900

    [update] practice

--- src/com/company/practice/programmers/lecture/DFS.java
+++ src/com/company/practice/programmers/lecture/DFS.java
@@ -1,18 +1,16 @@
 package com.company.practice.programmers.lecture;
 
 public class DFS {
-    static int[] valCount;
-    static int count = 0;
+    static int answer = 0;
     public static void main(String[] args) {
         int n = 2;
 
-        valCount = new int[]{n, n};
         dfs(n, n);
-        System.out.println(count);
+        System.out.println(answer);
     }
     public static void dfs(int open, int close) {
         if(open == 0 && close == 0) {
-            count++;
+            answer++;
             return;
         }
 
@@ -29,4 +27,34 @@ public class DFS {
         }
 
     }
+
+    /**
+     * 내 풀이
+     */
+    /*static int answer = 0;
+    public static void main(String[] args) {
+        int n = 2;
+
+        dfs(n, n);
+        System.out.println(answer);
+    }
+    public static void dfs(int open, int close) {
+        if(open == 0 && close == 0) {
+            answer++;
+            return;
+        }
+
+        if(open < close) {
+            if(open != 0) {
+                dfs(open - 1, close);
+            }
+            if(close != 0) {
+                dfs(open, close - 1);
+            }
+        } else { //open >= close
+            dfs(open - 1, close);
+
+        }
+
+    }*/
 }
