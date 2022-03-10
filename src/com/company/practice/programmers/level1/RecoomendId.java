919e8e4e151538df7455e5989836b70b26da3b08
innjie <innjie@naver.com>
2022-03-11 00:44:48 +0900

    [update] practice

--- src/com/company/practice/programmers/level1/RecoomendId.java
+++ src/com/company/practice/programmers/level1/RecoomendId.java
@@ -4,11 +4,12 @@ import java.util.Locale;
 
 public class RecoomendId {
     public static void main(String[] args) {
-        String new_id = "...!@BaT#*..y.abcdefghijklm";
+        String new_id = "abcdefghijklmn.p";
 
         String answer = "";
         //step 1
         answer = new_id.toLowerCase(Locale.ROOT);
+        System.out.println(answer);
 
         //step 2
         /**
@@ -18,24 +19,68 @@ public class RecoomendId {
 
         //step3
         int i = 0;
-        while(i < answer.length()) {
-            char curr = answer.charAt(i);
-            if(curr == '.') {
-                if(answer.charAt(i + 1) == '.') {
-                    answer = answer.substring(0, i) + answer.substring(i + 1, answer.length() - 1);
+        if (answer.length() >= 2) {
+            while (i < answer.length() - 1) {
+                char curr = answer.charAt(i);
+                if (curr == '.') {
+                    if (answer.charAt(i + 1) == '.') {
+                        answer = answer.substring(0, i) + answer.substring(i + 1, answer.length());
+                    } else {
+                        i++;
+                    }
+                } else {
+                    i++;
                 }
             }
-                i++;
-
         }
         System.out.println(answer);
+
         //step4
+        if(answer.startsWith(".")) {
+            if(answer.length() == 1) {
+                answer = "";
+            } else if(answer.length() == 2) {
+                answer = (String.valueOf(answer.charAt(1))) ;
+            } else {
+                answer = answer.substring(1, answer.length());
+            }
+        }
+        if(answer.endsWith(".")) {
+            if(answer.length() == 1) {
+                answer = "";
+            } else if(answer.length() == 2) {
+                answer = (String.valueOf(answer.charAt(0)));
+            } else {
+                answer = answer.substring(0, answer.length() - 1);
+            }
+        }
+        System.out.println(answer);
 
         //step5
+        if (answer.length() == 0) {
+            answer = "a";
+        }
+        System.out.println(answer);
 
         //step6
+        if (answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+        }
+        if(answer.endsWith(".")) {
+            if(answer.length() == 1) {
+                answer = "";
+            } else if(answer.length() == 2) {
+                answer = (String.valueOf(answer.charAt(0)));
+            } else {
+                answer = answer.substring(0, answer.length() - 1);
+            }
+        }
+        System.out.println(answer);
 
         //step7
-
+        while (answer.length() < 3) {
+            answer += answer.charAt(answer.length() - 1);
+        }
+        System.out.println(answer);
     }
 }
