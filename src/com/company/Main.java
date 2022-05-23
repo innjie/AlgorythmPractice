c573e98913caed56a508d2f8daa420f11ce1ae9b
innjie <innjie@naver.com>
2022-05-23 18:06:39 +0900

    [update] update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,39 @@
 package com.company;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        String s = "abebeaedeac";
+        int N = 5; //스테이지 수
+        int[] stages = {2, 1, 2, 6, 2, 4, 3, 3}; //도전중인 스테이지 번호
 
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        int[] clear = new int[N + 1];
+
+        for(int i = 0; i < stages.length; i++) {
+            clear[stages[i] - 1]++;
         }
 
-        Iterator<Map.Entry<Character, Integer>> entries = map.entrySet().iterator();
-        int answer = 0;
-        while (entries.hasNext()) {
-            Map.Entry<Character, Integer> entry = entries.next();
-            System.out.println(entry.getKey());
-            System.out.println(entry.getValue());
-            if (entry.getValue() % 2 == 1) {
-                answer++;
-
+        List<double[]> stageList = new ArrayList<>();
+        int total = stages.length;
+        for(int i = 0; i < clear.length - 1; i++) {
+            if(clear[i] == 0) {
+                stageList.add(new double[] {i, 0});
+            } else {
+                stageList.add(new double[] {i, clear[i] / (double)total});
             }
+            total -= clear[i];
         }
 
+        stageList.sort((a, b) -> Double.compare(b[1], a[1]));
+
+        int[] answer = new int[N];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = (int)stageList.get(i)[0] + 1;
+        }
+        for(int n : answer){
+            System.out.println(n);
+        }
 
     }
 }
