69ae70cb020923ca977052535fa1192e54f6436e
이인지 <innjie@naver.com>
2022-05-25 15:36:45 +0900

    [update] solve

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,32 +2,43 @@ package com.company;
 
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
-
+    static int total = 0;
     public static void main(String[] args) {
-        int left = 13;
-        int right = 17;
-
-
-        int answer = 0;
-        for(int i = left; i <= right; i++) {
-            int count = countOfDivides(i);
-
-            if(count % 2 == 0) {
-                answer += i;
-            } else {
-                answer -= i;
-            }
-        }
+        int[] nums = {1, 2, 3, 4};
+        int[] bucket = new int[3];
+        pick(nums, bucket, bucket.length);
+        System.out.println(total);
     }
-    public static int countOfDivides(int n ) {
-        int count = 0;
-        for(int i = 1; i <= n; i++) {
-            if(n % i == 0) {
-                count++;
+    public static void pick(int[] nums, int[] bucket, int k) {
+        if(k == 0) {
+            if(isPrime(Arrays.stream(bucket).sum())) {
+                total++;
+            }
+            return;
+        }
+
+        int lastIdx = bucket.length - k - 1;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(bucket.length == k) {
+                bucket[0] = nums[i];
+                pick(nums, bucket, k - 1);
+            } else if(bucket[lastIdx] < nums[i]) {
+                bucket[lastIdx + 1] = nums[i];
+                pick(nums, bucket, k - 1);
             }
         }
-        return count;
+
+    }
+    public static boolean isPrime(int n) {
+        for(int i = 2; i <= (int)Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
