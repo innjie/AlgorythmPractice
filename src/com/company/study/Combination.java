9ad541e440f65f6ace12f2089b10b3698a4e1469
이인지 <innjie@naver.com>
2022-05-25 15:16:20 +0900

    [update] solve

--- src/com/company/study/Combination.java
+++ src/com/company/study/Combination.java
@@ -2,11 +2,15 @@ package com.company.study;
 
 public class Combination {
     public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        int[] bucket = new int[3];
+        solution(nums, bucket, 4);
 
     }
-    public void solution(int[] items, int[] bucket, int k) {
+    public static void solution(int[] items, int[] bucket, int k) {
         if(k == 0) {
             //
+            return;
         }
         int lastIdx = bucket.length - k - 1;
         for(int i = 0; i < items.length; i++) {
