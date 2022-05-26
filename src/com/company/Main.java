3e3baffce446a24c436f36501957303a33e13c35
innjie <innjie@naver.com>
2022-05-26 20:07:10 +0900

    [update] update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,53 +1,34 @@
 package com.company;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
-    static int total = 0;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
+        Scanner scan = new Scanner(System.in);
 
-        for(int i = 0; i < N; i++) {
-            int num = scanner.nextInt();
-            int[] scores = new int[num];
-            for(int j = 0; j < num; j++) {
-                scores[j] = scanner.nextInt();
-            }
-            int total = Arrays.stream(scores).sum();
-            int average = total / num;
-            int check = (int) Arrays.stream(scores).filter(s -> s > average).count();
+        int testcase = scan.nextInt();
+        int[][] result = new int[testcase][3];
+        for(int i = 0; i < testcase; i++) {
+            result[i][0] = scan.nextInt();
+            result[i][1] = scan.nextInt();
+            result[i][2] = scan.nextInt();
+        }
 
-            System.out.println(String.format("%.3f", check / (double)num * 100) + "%");
+        for(int i = 0; i < testcase; i++) {
+            System.out.println(room(result[i][0], result[i][1], result[i][2]));
         }
     }
-    public static void pick(int[] nums, int[] bucket, int k) {
-        if(k == 0) {
-            if(isPrime(Arrays.stream(bucket).sum())) {
-                total++;
-            }
-            return;
-        }
+    public static int room(int H, int W, int n) {
+        int w = (n / H) + 1;
+        int h = (n % H);
 
-        int lastIdx = bucket.length - k - 1;
 
-        for(int i = 0; i < nums.length; i++) {
-            if(bucket.length == k) {
-                bucket[0] = nums[i];
-                pick(nums, bucket, k - 1);
-            } else if(bucket[lastIdx] < nums[i]) {
-                bucket[lastIdx + 1] = nums[i];
-                pick(nums, bucket, k - 1);
-            }
+        if(n % H == 0) {
+            w--;
+            h = H;
         }
-
-    }
-    public static boolean isPrime(int n) {
-        for(int i = 2; i <= (int)Math.sqrt(n); i++) {
-            if(n % i == 0) {
-                return false;
-            }
-        }
-        return true;
+        return h * 100 + w;
     }
 }
