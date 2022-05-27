cfd87b4f6ba8b32c4b2fdadec5efc43e77f3e28a
innjie <innjie@naver.com>
2022-05-27 14:27:15 +0900

    [update] update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,23 @@
 package com.company;
 
 import java.lang.reflect.Array;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        int a = 5, b = 24;
 
-        int testcase = scan.nextInt();
-        int[][] result = new int[testcase][3];
-        for(int i = 0; i < testcase; i++) {
-            result[i][0] = scan.nextInt();
-            result[i][1] = scan.nextInt();
-            result[i][2] = scan.nextInt();
-        }
-
-        for(int i = 0; i < testcase; i++) {
-            System.out.println(room(result[i][0], result[i][1], result[i][2]));
-        }
-    }
-    public static int room(int H, int W, int n) {
-        int w = (n / H) + 1;
-        int h = (n % H);
+        String[] days = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+        Map<String, Object> dayMap = new HashMap<>();
 
 
-        if(n % H == 0) {
-            w--;
-            h = H;
-        }
-        return h * 100 + w;
+        LocalDate dateTime = LocalDate.of(2016, a, b);
+        DayOfWeek dayOfWeek = dateTime.getDayOfWeek();
+        int dayNum = dayOfWeek.getValue();
+
+        System.out.println(days[dayNum - 1]);
     }
 }
